8025e85cb4bddbe969d8db51f79272acc7645026
Arvind Kumar C <arvindkumar.c@gmail.com>
2015-08-18 14:06:15 +0530

    create migrations table if not found. use that to track the migrations.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var AWS = require('aws-sdk'),
   fs = require('fs');
 
 module.exports = {
-  migrate: function() {
+  migrate: function(callback) {
     var migrationLocation = process.env.PWD + "/dynamo-migrations";
     fs.readdir(migrationLocation, function(err, files) {
       if (err) {
@@ -10,13 +10,64 @@ module.exports = {
       }
 
       var dynamodb = new AWS.DynamoDB({ endpoint: new AWS.Endpoint(process.env.AWS_DYNAMODB_ENDPOINT) });
-      files.forEach(function(filename) {
-        var fileLocation =  "./dynamo-migrations/" + filename;
-        var migration = require(fileLocation);
-        if (typeof migration.migrate === 'function') {
-          migration.migrate(dynamodb);
+      checkAndCreateMigrationTable(dynamodb, function(err) {
+        if (err) {
+          throw err;
+        }
+
+        files.forEach(function(filename) {
+          var fileLocation =  "./dynamo-migrations/" + filename;
+          var migration = require(fileLocation);
+          if (typeof migration.migrate === 'function') {
+            migration.migrate(dynamodb);
+          } else {
+            console.error("'migrate' function not found.");
+            throw new Error("'migrate' function not found in: ", fileLocation);
+          }
+        });
+
+        if (typeof callback === 'function') {
+          callback();
         }
       });
     });
   }
+}
+
+var checkAndCreateMigrationTable = function(dynamodb, callback) {
+  dynamodb.describeTable({TableName: 'dynamo_migrations'}, function(err, data) {
+    if (err != null && err.code == 'ResourceNotFoundException') {
+      var migTable = {
+        AttributeDefinitions: [
+            { AttributeName: 'id', AttributeType: 'N' },
+            { AttributeName: 'name', AttributeType: 'S' }
+          ],
+          GlobalSecondaryIndexes: [
+            {IndexName: 'name_index',
+             KeySchema: [{AttributeName: 'name', KeyType: 'HASH'}],
+             Projection: {ProjectionType: 'ALL'},
+             ProvisionedThroughput: {ReadCapacityUnits: 1, WriteCapacityUnits: 1}}
+          ],
+          KeySchema: [
+            { AttributeName: 'id', KeyType: 'HASH' }
+          ],
+          ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
+          // FIXME: 'dynamo_migrations' tablename should be customizable.
+          TableName: 'dynamo_migrations'
+      };
+
+      dynamodb.createTable(migTable, function(error, data) {
+        if (error) {
+            console.log("Migration table creation error: ", error, error.stack);
+            throw new Error("failed to create migrations table");
+          } else {
+            console.log("Migrations Table Created!");
+          }
+
+          callback(error);
+      });
+    }
+
+    callback(null);
+  });
 }
--- test/migrate.spec.js
+++ test/migrate.spec.js
@@ -1,10 +1,26 @@
-var should = require('chai').should(),
+var expect = require('chai').expect,
     dynmigrate = require('../index');
 
 require('dotenv').load();
 
 describe('#migrate', function() {
-  it('should run migrations for all file in location ./dynamo-migrations', function() {
+  it('should run migrations for all file in location ./dynamo-migrations', function(done) {
     dynmigrate.migrate();
+    done();
+  });
+
+  it('should create migrations table if not created', function(done) {
+    dynmigrate.migrate(function() {
+      var AWS = require('aws-sdk');
+      var dynamodb = new AWS.DynamoDB({ endpoint: new AWS.Endpoint(process.env.AWS_DYNAMODB_ENDPOINT) });
+      dynamodb.describeTable({TableName: 'dynamo_migrations'}, function(err, data) {
+        expect(err).to.be.null;
+        done();
+      });
+    });
+  });
+
+  xit('should run new migrations', function(done) {
+
   });
 });
